eb48d8042ae48001c66162283a8e3df3985c1a9b
Jayhiel vhon Lopez <jayhielvhonlopez@Jayhiels-MBP.example.com>
2022-03-30 16:03:51 +0800

    ontraq design

--- app/views/screens/Ontraq/OntraqScreen.js
+++ app/views/screens/Ontraq/OntraqScreen.js
@@ -1,11 +1,11 @@
 import React, {useContext, useState, useEffect} from 'react';
-import {View, Text, ScrollView} from 'react-native';
+import {View, Text, ScrollView, TouchableOpacity, RefreshControl} from 'react-native';
 import Student from '../../../api/Student';
 import Header from '../../../components/Header';
 import Loader from '../../../components/Loader';
 import {StudentContext} from '../../../context/StudentContext';
 import moment from 'moment';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import {AccordionList} from 'accordion-collapse-react-native';
 
 export default function Home() {
   const studentContext = useContext(StudentContext);
@@ -13,7 +13,6 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const [room, setRoom] = useState([]);
   const [attendances, setAttendances] = useState([]);
-  console.log({student});
 
   const getStundentAttenance = async () => {
     if (student != null) {
@@ -43,6 +42,83 @@ export default function Home() {
     }
   };
 
+  const _head = (item, index, isExpanded) => {
+    console.log({HEAD: item});
+    return (
+      <View
+        style={{
+          backgroundColor: isExpanded ? 'blue' : '#fff',
+          marginTop: 10,
+          padding: 10,
+          borderRadius: 10,
+          borderRadius: 5,
+        }}>
+        <Text
+          style={{
+            fontWeight: 'bold',
+            fontSize: 20,
+            color: isExpanded ? '#fff' : 'blue',
+          }}>
+          {item?.name}
+        </Text>
+      </View>
+    );
+  };
+
+  const _body = item => {
+    let displayRoom = attendances.filter(
+      attendance => attendance.venue.id == item.id,
+    );
+    return (
+      <View style={{padding: 10}}>
+        {displayRoom?.map((item, key) => {
+          return (
+            <View
+              key={key}
+              style={{
+                padding: 20,
+                borderRadius: 10,
+                backgroundColor: '#fff',
+                marginBottom: 10,
+                shadowColor: '#000',
+                shadowOffset: {
+                  width: 0,
+                  height: 2,
+                },
+                shadowOpacity: 0.25,
+                shadowRadius: 3.84,
+
+                elevation: 5,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}>
+              <Text
+                style={{
+                  backgroundColor:
+                    item.attendance_status == 'time_in' ? 'green' : 'red',
+                  padding: 10,
+                  color:
+                    item.attendance_status == 'time_in' ? '#fff' : '#fff',
+                  borderRadius: 10,
+                  fontWeight: 'bold',
+                }}>
+                {item.attendance_status == 'time_in'
+                  ? 'TIME IN'
+                  : 'TIME OUT'}
+              </Text>
+              <Text style={{fontSize: 16, color: '#000'}}>
+                {moment(item.created_at).format(
+                  'MMMM DD YYYY, h:mm:ss a',
+                )}
+              </Text>
+            </View>
+          );
+        })}
+      </View>
+    );
+  };
+
   useEffect(() => {
     getStundentAttenance();
   }, [student]);
@@ -50,7 +126,12 @@ export default function Home() {
   return (
     <View style={{flex: 1, marginBottom: 60}}>
       <Header />
-      <ScrollView>
+      <ScrollView refreshControl={
+          <RefreshControl
+            refreshing={loading}
+            onRefresh={() => getStundentAttenance()}
+          />
+        }>
         <View>
           {room.length <= 0 ? (
             <View>
@@ -58,8 +139,14 @@ export default function Home() {
             </View>
           ) : (
             <ScrollView>
-              <View>
-                {room.map((item, key) => {
+              <View style={{padding: 10}}>
+                <AccordionList
+                  list={room}
+                  header={_head}
+                  body={_body}
+                  expandedKey={0}
+                />
+                {/* {room.map((item, key) => {
                   console.log({DITO: item});
                   return (
                     <View key={key} style={{marginBottom: 12, padding: 10}}>
@@ -67,6 +154,7 @@ export default function Home() {
                         {item.name}
                       </Text>
                       <View>
+                      
                         {attendances
                           .filter(attendance => attendance.venue.id == item.id)
                           .map((attendance, key) => {
@@ -118,9 +206,10 @@ export default function Home() {
                             );
                           })}
                       </View>
+                     
                     </View>
                   );
-                })}
+                })} */}
               </View>
             </ScrollView>
           )}
