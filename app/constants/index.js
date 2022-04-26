5a4d6fae0b71a3052d8125af8d9e11a5d238d5de
Laurence Bautista <rorens05@gmail.com>
2022-04-26 16:31:25 +0800

    Change url

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,6 +1,6 @@
 // export const DEV_API_URL = 'https://api-parentline.tekteachlms.com/'
-// export const DEV_API_URL = 'http://199.231.160.83:3000/'
-export const DEV_API_URL = 'http://10.0.2.2:3000/'
+export const DEV_API_URL = 'http://199.231.160.83:3000/'
+// export const DEV_API_URL = 'http://10.0.2.2:3000/'
 
 //LMS
 export const DEV_LMS_API_URL = 'https://dev-api-v3.tekteachlms.com/'
