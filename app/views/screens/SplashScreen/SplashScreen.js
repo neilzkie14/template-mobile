022c05934053718922237a21ac9795065b7a0880
Adonis Sarmiento <sarmiento.vam@gmail.com>
2022-10-03 14:52:18 +0800

    change navigate to replace SplashScreen navigation

--- app/views/screens/SplashScreen/SplashScreen.js
+++ app/views/screens/SplashScreen/SplashScreen.js
@@ -20,9 +20,9 @@ export default function SplashScreen() {
     if(response.ok){
       await refreshUser();
       await refreshStudent();
-      navigation.navigate('Dashboard')
+      navigation.replace('Dashboard')
     }else{
-      navigation.navigate('Login')
+      navigation.replace('Login')
     }
   }
 
