9e9cf07486c6ab70beccd4d02b228c065f861e50
Laurence Bautista <rorens05@gmail.com>
2022-10-21 17:55:51 +0800

    Update endpoints

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,9 +1,9 @@
 /** DEVELOPMENT */
-export const DEV_API_URL = 'http://10.0.2.2:3000/';
-export const SOCKET_URL = 'ws://10.0.2.2:3000/cable';
-export const DEV_LMS_API_URL = 'https://dev-api-v3.tekteachlms.com/';
+// export const DEV_API_URL = 'http://10.0.2.2:3000/';
+// export const SOCKET_URL = 'ws://10.0.2.2:3000/cable';
+// export const DEV_LMS_API_URL = 'https://dev-api-v3.tekteachlms.com/';
 
 /** PRODUCTION */
-// export const DEV_API_URL = 'http://199.231.160.83:3000/';
-// export const SOCKET_URL = 'ws://199.231.160.83:3000/cable';
-// export const DEV_LMS_API_URL = 'https://dev-api-v3.tekteachlms.com/'
+export const DEV_API_URL = 'http://199.231.160.83:3000/';
+export const SOCKET_URL = 'ws://199.231.160.83:3000/cable';
+export const DEV_LMS_API_URL = 'https://dev-api-v3.tekteachlms.com/';
