082b96e026db124a467801bdf1b655463c697662
Adonis Sarmiento <sarmiento.vam@gmail.com>
2022-10-26 10:54:43 +0800

    Fix Birthday Validation

--- app/views/screens/Settings/components/SecuritySettings.js
+++ app/views/screens/Settings/components/SecuritySettings.js
@@ -6,12 +6,54 @@ import Loader from '../../../../components/Loader';
 import arrow from '../../../../images/arrow.png'
 import { UserContext } from '../../../../context/UserContext';
 import { getParams } from '../../../../utils/navigation_helper';
+import Auth from '../../../../api/Auth';
 export default function SecuritySettings({ }) {
   const navigation = useContext(NavigationContext);
   const [loading, setLoading] = useState(false);
   const userContext = useContext(UserContext);
   const params = getParams(navigation);
   const { user } = userContext.data;
+
+  const showAlert = () => {
+    Alert.alert(
+      'Important!',
+      'Are you sure you really want to delete your account?',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => {},
+          style: 'cancel',
+        },
+        {
+          text: 'Confirm',
+          onPress: () => {
+            confirmDeletion();
+          },
+        },
+      ],
+      'secure-text',
+    );
+  };
+
+  const confirmDeletion = async () => {
+    let response = await new Auth().accountDeletion();
+    if (response.ok) {
+    } else {
+      alert('Something went wrong in fetching Messages');
+    }
+    try {
+      await AsyncStorage.removeItem('token');
+      await AsyncStorage.clear();
+      navigation.dispatch(
+        CommonActions.reset({
+          index: 0,
+          routes: [{name: 'SplashScreen'}],
+        }),
+      );
+    } catch (error) {
+      console.log({error});
+    }
+  };
 
   return (
     <View style={{ flex: 1, backgroundColor: '#FFFFFF' }}>
@@ -64,6 +106,11 @@ export default function SecuritySettings({ }) {
             }}
             resizeMethod="resize"
           />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => showAlert()} style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 15, alignItems: 'center' }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Text style={{ fontSize: 16, paddingLeft: 10, color: '#D82E2E' }}>{'Delete Account'}</Text>
         </View>
       </TouchableOpacity>
     </View>
