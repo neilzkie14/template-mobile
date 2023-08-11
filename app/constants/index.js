0ad3df53449160cdd48aed7c65b222487cdff507
ChromneilPedeglorio <neilzkie2@gmail.com>
2023-08-11 14:53:34 +0800

    Change base url port

--- app/constants/index.js
+++ app/constants/index.js
@@ -4,6 +4,6 @@
 // export const DEV_LMS_API_URL = 'https://dev-api-v3.tekteachlms.com/';
 
 /** PRODUCTION */
-export const DEV_API_URL = 'http://199.231.160.83:3000/';
-export const SOCKET_URL = 'ws://199.231.160.83:3000/cable';
+export const DEV_API_URL = 'http://144.168.40.89:3000/';
+export const SOCKET_URL = 'ws://144.168.40.89:3000/cable';
 export const DEV_LMS_API_URL = 'https://dev-api-v3.tekteachlms.com/';
